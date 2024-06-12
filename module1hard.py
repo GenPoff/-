b2e837e27d610fc19c098b05defe24d8059e2d80
GenPoff <mylene.ua@mail.ru>
2024-06-12 12:36:27 +0300

    Reviewed task

--- module1hard.py
+++ module1hard.py
@@ -1,31 +1,19 @@
+# Список оценок
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+# Множество имен учеников
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-print(students)
-print(type(students))
-students2 = list(students)
-print(type(students2))
-students2.sort()    # нашла такую сортировку, ибо до другой не смогла додуматься
-print(students2)
+# Список имен учеников в алфавитном порядке
+students = sorted(students)
 
-print(type(grades))
-print(len(grades))
-a = len(grades[0])
-b = len(grades[1])
-c = len(grades[2])
-d = len(grades[3])
-e = len(grades[4])
-print(a, b, c, d, e)
+# Словарь для хранения среднего балла каждого ученика
+average_grades = {}
 
-a = sum(grades[0])/a
-b = sum(grades[1])/b
-c = sum(grades[2])/c
-d = sum(grades[3])/d
-e = sum(grades[4])/e
-print(a, b, c, d, e)
+# Перебираем имена учеников
+for student in students:  # Отсортированный список имен учеников
+    total_score = sum(grades[students.index(student)])  # Сумма оценок текущего ученика
+    count = len(grades[students.index(student)])        # Количество оценок текущего ученика
+    average_grade = total_score / count                 # Средний балл текущего ученика
 
-print({students2[0]: a, students2[1]: b, students2[2]: c, students2[3]: d, students2[4]: e})
+    average_grades[student] = average_grade
 
-
-
-
-
+print(average_grades)
